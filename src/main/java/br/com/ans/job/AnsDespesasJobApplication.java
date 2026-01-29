b59fd46b8e17a74947795880a6ebc8a3fca4dc0c
Joao Victor <jvictorsousa07@outlook.com>
2026-01-29 16:00:45 -0300

    feat: setup inicial do projeto e configuracao de banco de dados

---
+++ src/main/java/br/com/ans/job/AnsDespesasJobApplication.java
@@ -0,0 +1,13 @@
+package br.com.ans.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsDespesasJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsDespesasJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ans/job/AnsDespesasJobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ans.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsDespesasJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
